14e2c628a0611d3058ed0d15a274714025a50b5b
Shanavas M <shanavas.m2@gmail.com>
2017-05-09 20:38:15 +0300

    Add ansible layer for ninja2 support

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -24,6 +24,7 @@ values."
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
      ;; better-defaults
+     ansible
      graphviz
      auto-completion
      c-c++
