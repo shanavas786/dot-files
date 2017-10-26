647ea53cacfcc5a2e5ca2064e229b8f237a54358
Shanavas M <shanavas@disroot.org>
2017-10-26 15:01:39 +0300

    Change theme name to dark

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -136,7 +136,7 @@ values."
     ;; Press <SPC> T n to cycle to the next theme in the list (works great
     ;; with 2 themes variants, one dark and one light)
     dotspacemacs-themes '(spacemacs-dark
-                           farmhouse
+                           farmhouse-dark
                            deeper-blue
                            dracula
                            manoj-dark
