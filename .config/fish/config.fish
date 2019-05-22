2e206766bfba3dbd74029eb2736382a93ffa25c3
Shanavas M <shanavas.m2@gmail.com>
2019-05-22 10:59:56 +0530

    update configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,12 @@
 set PATH $HOME/.cargo/bin $PATH
 
-feh --bg-fill ~/.config/i3/background.png
+if command -v exa > /dev/null
+   abbr -a l 'exa'
+   abbr -a ls 'exa'
+   abbr -a ll 'exa -l'
+   abbr -a lll 'exa -la'
+else
+   abbr -a l 'ls'
+   abbr -a ll 'ls -l'
+   abbr -a lll 'ls -la'
+end
