6e94faa0463d0fa9d4da3d0c3a6c501ce10a18ca
Shanavas M <shanavas.m2@gmail.com>
2019-05-23 12:46:27 +0530

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,31 @@ else
    abbr -a ll 'ls -l'
    abbr -a lll 'ls -la'
 end
+
+
+# Fish git prompt
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate ''
+set __fish_git_prompt_showupstream 'none'
+set -g fish_prompt_pwd_dir_length 2
+
+
+function fish_prompt
+   set_color blue
+   echo -n (hostname)
+   if [ $PWD != $HOME ]
+      set_color brblack
+      echo -n ':'
+      set_color yellow
+      echo -n (prompt_pwd)
+   end
+   set_color green
+   printf '%s ' (__fish_git_prompt)
+   set_color red
+   echo -n '| '
+   set_color normal
+end
+
+# no more geeting
+set fish_greeting
