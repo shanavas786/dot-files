46f13cff16252607f5fee4f9e6325d37c95b43cc
Shanavas M <shanavas.m2@gmail.com>
2025-07-11 20:01:09 +0530

    Add ruff and uv

--- install.sh
+++ install.sh
@@ -8,6 +8,14 @@ sudo apt install git vim emacs stow build-essential curl wget fonts-inconsolata 
 # install rust toolchain
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+# ruff
+curl -LsSf https://astral.sh/ruff/install.sh | sh
+
+# uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# dockfmt
+
 stow config -t ~/.config
 stow bash -t ~
 stow editorconfig -t ~
